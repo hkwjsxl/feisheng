5cc4f7cfb9fd99c63418db15ec22fb505ee8d19d
hankewei <hankewei0224@163.com>
2023-03-16 20:50:09 +0800

    feature:客户端使用本地存储保存token；

--- fsweb/src/components/Login.vue
+++ fsweb/src/components/Login.vue
@@ -15,148 +15,175 @@
       <p>忘记密码</p>
     </div>
     <button class="login_btn" @click="loginhandler">登录</button>
-    <p class="go_login" >没有账号 <span>立即注册</span></p>
+    <p class="go_login">没有账号 <span>立即注册</span></p>
   </div>
   <div class="inp" v-show="user.login_type===1">
     <input v-model="user.mobile" type="text" placeholder="手机号码" class="user">
-    <input v-model="user.code"  type="text" class="code" placeholder="短信验证码">
+    <input v-model="user.code" type="text" class="code" placeholder="短信验证码">
     <el-button id="get_code" type="primary">获取验证码</el-button>
     <button class="login_btn">登录</button>
-    <p class="go_login" >没有账号 <span>立即注册</span></p>
+    <p class="go_login">没有账号 <span>立即注册</span></p>
   </div>
 </template>
 
 <script setup>
 import user from "../api/user";
-import { ElMessage } from 'element-plus'
+import {ElMessage} from 'element-plus'
 
 // 登录处理
-const loginhandler = ()=>{
-  // 验证数据
-  if(user.account.length<1 || user.password.length<1){
+const loginhandler = () => {
+  if (user.account.length < 1 || user.password.length < 1) {
     // 错误提示
     console.log("用户名或密码不能为空！");
-    ElMessage.error("用户名或密码不能为空！");
-    return ;
+    ElMessage.error('用户名或密码不能为空！');
+    return;  // 在函数/方法中，可以阻止代码继续往下执行
   }
 
-  // 登录请求处理
-  user.login().then(response=>{
-    console.log(response.data);
+  // 发送请求
+  user.login({
+    username: user.account,
+    password: user.password
+  }).then(response => {
+    // 保存token，并根据用户的选择，是否记住密码
+    localStorage.removeItem("token");
+    sessionStorage.removeItem("token");
+    console.log(response.data.token);
+    if (user.remember) { // 判断是否记住登录状态
+      // 记住登录
+      localStorage.token = response.data.token
+    } else {
+      // 不记住登录，关闭浏览器以后就删除状态
+      sessionStorage.token = response.data.token;
+    }
+    // 保存token，并根据用户的选择，是否记住密码
+    // 成功提示
     ElMessage.success("登录成功！");
-  }).catch(error=>{
-    ElMessage.error("登录失败！");
+    console.log("登录成功！");
+    // 关闭登录弹窗
+  }).catch(error => {
+    console.log(error);
   })
 }
 
 </script>
 
 <style scoped>
-.title{
-    font-size: 20px;
-    color: #9b9b9b;
-    letter-spacing: .32px;
-    border-bottom: 1px solid #e6e6e6;
-    display: flex;
-    justify-content: space-around;
-    padding: 0 60px 0 60px;
-    margin-bottom: 20px;
-    cursor: pointer;
-}
-.title span.active{
-	color: #4a4a4a;
-    border-bottom: 2px solid #84cc39;
+.title {
+  font-size: 20px;
+  color: #9b9b9b;
+  letter-spacing: .32px;
+  border-bottom: 1px solid #e6e6e6;
+  display: flex;
+  justify-content: space-around;
+  padding: 0 60px 0 60px;
+  margin-bottom: 20px;
+  cursor: pointer;
 }
 
-.inp{
-	width: 350px;
-	margin: 0 auto;
-}
-.inp .code{
-    width: 220px;
-    margin-right: 16px;
-}
-#get_code{
-   margin-top: 6px;
-}
-.inp input{
-    outline: 0;
-    width: 100%;
-    height: 45px;
-    border-radius: 4px;
-    border: 1px solid #d9d9d9;
-    text-indent: 20px;
-    font-size: 14px;
-    background: #fff !important;
-}
-.inp input.user{
-    margin-bottom: 16px;
-}
-.inp .rember{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    position: relative;
-    margin-top: 10px;
-}
-.inp .rember p:first-of-type{
-    font-size: 12px;
-    color: #4a4a4a;
-    letter-spacing: .19px;
-    margin-left: 22px;
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-align: center;
-    align-items: center;
-    /*position: relative;*/
-}
-.inp .rember p:nth-of-type(2){
-    font-size: 14px;
-    color: #9b9b9b;
-    letter-spacing: .19px;
-    cursor: pointer;
+.title span.active {
+  color: #4a4a4a;
+  border-bottom: 2px solid #84cc39;
 }
 
-.inp .rember input{
-    outline: 0;
-    width: 15px;
-    height: 30px;
-    border-radius: 4px;
-    border: 1px solid #d9d9d9;
-    text-indent: 20px;
-    font-size: 14px;
-    background: #fff !important;
-    vertical-align: middle;
-    margin-right: 4px;
+.inp {
+  width: 350px;
+  margin: 0 auto;
 }
 
-.inp .rember p span{
-    display: inline-block;
-    font-size: 12px;
-    width: 100px;
+.inp .code {
+  width: 220px;
+  margin-right: 16px;
 }
-.login_btn{
-    cursor: pointer;
-    width: 100%;
-    height: 45px;
-    background: #84cc39;
-    border-radius: 5px;
-    font-size: 16px;
-    color: #fff;
-    letter-spacing: .26px;
-    margin-top: 30px;
-    border: none;
-    outline: none;
+
+#get_code {
+  margin-top: 6px;
 }
-.inp .go_login{
-    text-align: center;
-    font-size: 14px;
-    color: #9b9b9b;
-    letter-spacing: .26px;
-    padding-top: 20px;
+
+.inp input {
+  outline: 0;
+  width: 100%;
+  height: 45px;
+  border-radius: 4px;
+  border: 1px solid #d9d9d9;
+  text-indent: 20px;
+  font-size: 14px;
+  background: #fff !important;
 }
-.inp .go_login span{
-    color: #84cc39;
-    cursor: pointer;
+
+.inp input.user {
+  margin-bottom: 16px;
+}
+
+.inp .rember {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  margin-top: 10px;
+}
+
+.inp .rember p:first-of-type {
+  font-size: 12px;
+  color: #4a4a4a;
+  letter-spacing: .19px;
+  margin-left: 22px;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  /*position: relative;*/
+}
+
+.inp .rember p:nth-of-type(2) {
+  font-size: 14px;
+  color: #9b9b9b;
+  letter-spacing: .19px;
+  cursor: pointer;
+}
+
+.inp .rember input {
+  outline: 0;
+  width: 15px;
+  height: 30px;
+  border-radius: 4px;
+  border: 1px solid #d9d9d9;
+  text-indent: 20px;
+  font-size: 14px;
+  background: #fff !important;
+  vertical-align: middle;
+  margin-right: 4px;
+}
+
+.inp .rember p span {
+  display: inline-block;
+  font-size: 12px;
+  width: 100px;
+}
+
+.login_btn {
+  cursor: pointer;
+  width: 100%;
+  height: 45px;
+  background: #84cc39;
+  border-radius: 5px;
+  font-size: 16px;
+  color: #fff;
+  letter-spacing: .26px;
+  margin-top: 30px;
+  border: none;
+  outline: none;
+}
+
+.inp .go_login {
+  text-align: center;
+  font-size: 14px;
+  color: #9b9b9b;
+  letter-spacing: .26px;
+  padding-top: 20px;
+}
+
+.inp .go_login span {
+  color: #84cc39;
+  cursor: pointer;
 }
 </style>
