e5a44f13f3e36e4824c33ca20edd4f11587d2255
hankewei <hankewei0224@163.com>
2023-04-04 22:13:20 +0800

    feature：把订单子应用相关的模型注册到admin管理站点

--- fsapi/fsapi/apps/orders/admin.py
+++ fsapi/fsapi/apps/orders/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Order, OrderDetail
 
-# Register your models here.
+
+# class OrderDetailInLine(admin.StackedInline):
+class OrderDetailInLine(admin.TabularInline):
+    """订单项的内嵌类"""
+    model = OrderDetail
+    fields = ["course", "price", "real_price", "discount_name"]
+    # readonly_fields = ["discount_name"]
+
+
+class OrderModelAdmin(admin.ModelAdmin):
+    """订单信息的模型管理器"""
+    list_display = ["id", "order_number", "user", "total_price", "total_price", "order_status"]
+    inlines = [OrderDetailInLine, ]
+
+
+admin.site.register(Order, OrderModelAdmin)
--- fsapi/fsapi/apps/orders/apps.py
+++ fsapi/fsapi/apps/orders/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
-    verbose_name = "订单"
+    verbose_name = "订单管理"
     verbose_name_plural = verbose_name
