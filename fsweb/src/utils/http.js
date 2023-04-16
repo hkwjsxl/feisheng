22a22a0a886592d560b08ffbac6590a070aec0c4
hankewei <hankewei0224@163.com>
2023-04-16 17:27:13 +0800

    feature: 客户端调整api服务端地址

--- fsweb/src/utils/http.js
+++ fsweb/src/utils/http.js
@@ -1,8 +1,9 @@
 import axios from "axios"
+import settings from "../settings";
 
 const http = axios.create({
     // timeout: 2500,  // 请求超时，有大文件上传需要关闭这个配置
-    baseURL: "http://127.0.0.1:8000",  // 设置api服务端的默认地址[如果基于服务端实现的跨域，这里可以填写api服务端的地址，如果基于nodejs客户端测试服务器实现的跨域，则这里不能填写api服务端地址]
+    baseURL: settings.host,  // 设置api服务端的默认地址[如果基于服务端实现的跨域，这里可以填写api服务端的地址，如果基于nodejs客户端测试服务器实现的跨域，则这里不能填写api服务端地址]
     withCredentials: false,  // 是否允许客户端ajax请求时携带cookie
 })
 
